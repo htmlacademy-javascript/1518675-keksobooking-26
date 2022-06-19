2d32ca2a7d0833cebe4d55282867024b6ea3a869
Keks <keks@htmlacademy.ru>
2022-06-19 17:59:50 +0300

    :heavy_check_mark: Сборка #3

--- 3/js/main.js
+++ 3/js/main.js
@@ -1,73 +1,65 @@
+// Константы
+const OBJECT_TYPES = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
+const CHECK_IN_TIME = ['12:00', '13:00', '14:00'];
+const CHECK_OUT_TIME = ['12:00', '13:00', '14:00'];
+const FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+const MAX_PRICE = 6000000;
+const lat = { MIN: 35.65000, MAX: 35.70000 };
+const lng = { MIN: 139.70000, MAX: 139.80000 };
+const DIGITS_COORDINATE = 5;
+const OBJECTS_AMOUNT = 10;
+
 // Функция возвращающает случайное число с плавающей точкой из переданного диапазона включительно
-
 const getRandomPositiveFloat = (a, b, digits = 1) => {
-
   const lower = Math.min(Math.abs(a), Math.abs(b));
   const upper = Math.max(Math.abs(a), Math.abs(b));
-
   const result = Math.random() * (upper - lower) + lower;
 
   return +result.toFixed(digits);
 };
 
-// Функция генерирует объект с описанием авторов
-const generateAuthorObj = (obj = {}) => {
-  for (let i = 1; i <= 10; i++) {
-    obj[i] = {};
-    obj[i].avatar = `img/avatars/user${(i < 10) ? `0${i}` : i}.png`;
-  }
+// Функция возвращает случайный элемент из массива
+const getRandomArrayElement = (elements) => elements[getRandomPositiveFloat(0, elements.length - 1, 0)];
 
-  return obj;
+// Функция возвращает случайные координаты из заданного диапазона
+const getRandomCoordinate = (coordinate, digits) => getRandomPositiveFloat(coordinate.MIN, coordinate.MAX, digits);
+
+// Функция генерирует объект с описанием авторов
+let i = 0;
+const generateAuthorObj = () => {
+  i++;
+  return {
+    avatar: `img/avatars/user${(i < OBJECTS_AMOUNT) ? `0${i}` : i}.png`
+  };
 };
 
 // Функция генерирует объект с информацией об объявлениях
-const OBJECT_TYPES = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
-const CHECK_IN_TIME = ['12:00', '13:00', '14:00'];
-const CHECK_OUT_TIME = ['12:00', '13:00', '14:00'];
-const FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+const generateOfferObj = () => ({
+  title: 'Кликбейт заголовок',
+  address: `${getRandomCoordinate(lat, DIGITS_COORDINATE)}, ${getRandomCoordinate(lng, DIGITS_COORDINATE)}`,
+  price: getRandomPositiveFloat(0, MAX_PRICE, 0),
+  type: getRandomArrayElement(OBJECT_TYPES),
+  rooms: getRandomPositiveFloat(0, 6, 0),
+  guests: getRandomPositiveFloat(0, 25, 0),
+  checkin: getRandomArrayElement(CHECK_IN_TIME),
+  checkout: getRandomArrayElement(CHECK_OUT_TIME),
+  features: FEATURES.slice(getRandomPositiveFloat(0, FEATURES.length - 1, 0)).sort(() => Math.random() - 0.5),
+  description: 'Супер крутое жильё',
+  photos: ['https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
+    'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
+    'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg']
+});
 
-const generateOfferObj = (obj = {}) => {
-  for (let i = 1; i <= 10; i++) {
-    obj[i] = {};
-    obj[i].title = 'Кликбейт заголовок';
-    obj[i].address = {};
-    obj[i].address.location = {};
-    obj[i].address.location.lat = '8582162';
-    obj[i].address.location.lng = '2186005';
-    obj[i].price = getRandomPositiveFloat(0, 20000, 0);
-    obj[i].type = OBJECT_TYPES[getRandomPositiveFloat(0, OBJECT_TYPES.length - 1, 0)];
-    obj[i].rooms = getRandomPositiveFloat(0, 6, 0);
-    obj[i].guests = getRandomPositiveFloat(0, 25, 0);
-    obj[i].checkin = CHECK_IN_TIME[getRandomPositiveFloat(0, CHECK_IN_TIME.length - 1, 0)];
-    obj[i].checkout = CHECK_OUT_TIME[getRandomPositiveFloat(0, CHECK_OUT_TIME.length - 1, 0)];
+// Функция генерирует объект с информацией о местоположении в виде географических координат
+const generateLocationObj = () => ({
+  lat: getRandomCoordinate(lat, DIGITS_COORDINATE),
+  lng: getRandomCoordinate(lng, DIGITS_COORDINATE)
+});
 
-    // Клонирование массива, чтобы объекты features были разные в каждой итерации
-    const cloneFeatures = Object.assign([], FEATURES);
-    obj[i].features = cloneFeatures.sort(() => Math.random() - 0.5);
-    obj[i].description = 'Супер крутое жильё. 10/10';
-    obj[i].photos = ['https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
-      'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
-      'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg'];
-  }
+const authorsObj = Array.from({length: OBJECTS_AMOUNT}, generateAuthorObj);
+const offersObj = Array.from({length: OBJECTS_AMOUNT}, generateOfferObj);
+const locationsObj = Array.from({length: OBJECTS_AMOUNT}, generateLocationObj);
 
-  return obj;
-};
-
-// Функция генерирует объект с информации о местоположении в виде географических координат
-const generateLocationObj = (obj = {}) => {
-  for (let i = 1; i <= 10; i++) {
-    obj[i] = {};
-    obj[i].lat = `35.${getRandomPositiveFloat(65, 70, 0)}00`;
-    obj[i].lng = `139.${getRandomPositiveFloat(70, 80, 0)}00`;
-  }
-
-  return obj;
-};
-
-const authorObj = generateAuthorObj();
-const offerObj = generateOfferObj();
-const locationObj = generateLocationObj();
-
-console.log(authorObj); // eslint-disable-line no-console
-console.log(offerObj); // eslint-disable-line no-console
-console.log(locationObj); // eslint-disable-line no-console
+console.log(authorsObj); // eslint-disable-line no-console
+console.log(offersObj); // eslint-disable-line no-console
+console.log(locationsObj); // eslint-disable-line no-console
