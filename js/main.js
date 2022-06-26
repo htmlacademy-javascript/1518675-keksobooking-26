5f0f1f2f57192b48a9956a04166da6cf2d427d68
gladosq <gladosq@gmail.com>
2022-06-26 20:52:07 +0400

    module7-task1

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
 import {createData} from './data.js';
 import {createCards} from './create-elements.js';
+import {disableForm, enableForm} from './form.js';
 
 const mocks = createData();
 
 createCards(mocks.slice(0, 1));
+
+disableForm();
+enableForm();
