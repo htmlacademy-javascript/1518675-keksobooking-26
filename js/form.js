d5750adbc9632f894f1cad4a6f64b8b70f7ccd9c
gladosq <gladosq@gmail.com>
2022-06-27 19:46:54 +0400

    fixed form

--- js/form.js
+++ js/form.js
@@ -5,12 +5,16 @@ const mapFilters = document.querySelector('.map__filters');
 const disableForm = () => {
   formInfo.classList.toggle('ad-form--disabled');
   mapFilters.classList.toggle('map__filters--disabled');
+  mapFilters.querySelectorAll('select').forEach((item) => { item.disabled = true; });
+  mapFilters.querySelector('fieldset').disabled = true;
   formFieldsets.forEach((item) => { item.disabled = true; });
 };
 
 const enableForm = () => {
   formInfo.classList.toggle('ad-form--disabled');
   mapFilters.classList.toggle('map__filters--disabled');
+  mapFilters.querySelectorAll('select').forEach((item) => { item.disabled = false; });
+  mapFilters.querySelector('fieldset').disabled = false;
   formFieldsets.forEach((item) => { item.disabled = false; });
 };
 
