9825b6867d9268a627ebaa51fb5b058b471f237f
Keks <keks@htmlacademy.ru>
2022-06-27 18:05:41 +0300

    :heavy_check_mark: Сборка #7

--- 7/js/form.js
+++ 7/js/form.js
@@ -1,13 +1,16 @@
 const formInfo = document.querySelector('.ad-form');
 const formFieldsets = document.querySelectorAll('.ad-form fieldset');
+const mapFilters = document.querySelector('.map__filters');
 
 const disableForm = () => {
   formInfo.classList.toggle('ad-form--disabled');
+  mapFilters.classList.toggle('map__filters--disabled');
   formFieldsets.forEach((item) => { item.disabled = true; });
 };
 
 const enableForm = () => {
   formInfo.classList.toggle('ad-form--disabled');
+  mapFilters.classList.toggle('map__filters--disabled');
   formFieldsets.forEach((item) => { item.disabled = false; });
 };
 
