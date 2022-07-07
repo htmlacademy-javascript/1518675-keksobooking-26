cd9f22918eff11633a435e289e610b800e588b28
gladosq <gladosq@gmail.com>
2022-07-07 19:30:36 +0400

    module9-task1

--- js/map.js
+++ js/map.js
@@ -1,11 +1,12 @@
 import {enableForm} from './form.js';
-import {getNounPluralForm} from './utils.js';
-import {OBJECT_TYPES} from './data.js';
+import {createPopup} from './create-popup.js';
 
 const CENTER_OF_TOKYO = {
   lat: 35.68950,
   lng: 139.69171,
 };
+
+const MAX_DIGIT = 5;
 
 // Создание карты
 const map = L.map('map-canvas')
@@ -54,46 +55,13 @@ marker.addTo(map);
 // Функция подставновки координт главного пина в значение input
 const fillAddressInput = (obj) => {
   const address = document.querySelector('#address');
-  address.value = `${obj.lat}, ${obj.lng}`;
+  address.value = `${obj.lat.toFixed(MAX_DIGIT)}, ${obj.lng.toFixed(MAX_DIGIT)}`;
 };
 
 marker.on('moveend', (evt) => {
   const coordinates = evt.target.getLatLng();
   fillAddressInput(coordinates);
 });
-
-// Функция создания разметки из шаблона для всплывающего попапа
-const createPopup = ({author, offer}) => {
-  const popupTemplate = document.querySelector('#card').content.querySelector('.popup');
-  const popup = popupTemplate.cloneNode(true);
-
-  popup.querySelector('.popup__avatar').src = `${author.avatar}`;
-  popup.querySelector('.popup__title').textContent = offer.title;
-  popup.querySelector('.popup__text--address').textContent = `${offer.address}`;
-  popup.querySelector('.popup__text--price').innerHTML = `${offer.price} <span>₽/ночь</span>`;
-  popup.querySelector('.popup__type').textContent = OBJECT_TYPES[offer.type].name;
-  popup.querySelector('.popup__text--capacity').textContent = `
-    ${offer.rooms} ${getNounPluralForm(offer.rooms, 'комната', 'комнаты', 'комнат')} для
-    ${offer.guests} ${getNounPluralForm(offer.guests, 'гостя', 'гостей', 'гостей')}
-  `;
-  popup.querySelector('.popup__text--time').textContent = `Заезд после ${offer.checkin}, выезд до ${offer.checkout}`;
-
-  const featuresContainer = popup.querySelector('.popup__features');
-  featuresContainer.innerHTML = '';
-  offer.features.forEach((feature) => {
-    featuresContainer.innerHTML += `<li class="popup__feature popup__feature--${feature}"></li>`;
-  });
-
-  popup.querySelector('.popup__description').textContent = `${offer.description}`;
-
-  const photosContainer = popup.querySelector('.popup__photos');
-  photosContainer.innerHTML = '';
-  offer.photos.forEach((photo) => {
-    photosContainer.innerHTML += `<img src="${photo}" class="popup__photo" width="45" height="40" alt="Фотография жилья">`;
-  });
-
-  return popup;
-};
 
 const markerGroup = L.layerGroup().addTo(map);
 
